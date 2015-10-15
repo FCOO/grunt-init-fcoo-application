2040fff530f060cc2fb936526e368b74513deae5
NielsHolt <niels@steenbuchholt.dk>
2015-10-15 22:03:38 +0200

    Version 1.0.0

--- template.js
+++ template.js
@@ -52,8 +52,10 @@ exports.template = function(grunt, init, done) {
 		props.licenses = ['MIT'];
 		props.year = (new Date()).getFullYear();
 
-    // Files to copy (and process).
+
+		// Files to copy (and process).
     var files = init.filesToCopy(props);
+
 
     // Add properly-named license files.
     init.addLicenseFiles(files, props.licenses);
@@ -61,7 +63,15 @@ exports.template = function(grunt, init, done) {
     // Actually copy (and process) files.
     init.copyAndProcess(files, props);
 
-    // All done!
+
+		//Copy gruntfile.js and package.json from gruntfile/ to root
+		var src_path = init.srcpath( '/../gruntfile/' );
+		init.copyAndProcess({
+				'package.json': src_path+'package.json', 
+				'gruntfile.js': src_path+'gruntfile.js' 
+		}, props );
+
+		// All done!
     done();
 	});
 
