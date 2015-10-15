ce61b66e23a313222bb174b7779720c1a15bec69
NielsHolt <niels@steenbuchholt.dk>
2015-10-15 15:10:40 +0200

    Test af submodules

---
+++ root/README.md
@@ -0,0 +1,39 @@
+# {%= name %}
+>{%= description %}
+
+
+## Description
+This application is ...
+
+## link (delete the wrong links!)
+
+
+    https://fcoo.dk/{%= name %}
+or
+
+    https://{%= name %}.fcoo.dk
+or
+
+    https://app.fcoo.dk/{%= name %}
+
+
+## Copyright and License
+
+Copyright (c) 2015 [FCOO](https://github.com/FCOO)
+
+## Contact information
+
+{%= author_name %} {%= author_email %}
+
+
+## Credits and acknowledgements
+
+
+## Known bugs
+
+## Troubleshooting
+
+## Changelog
+
+
+
---
+++ template.js
@@ -0,0 +1,68 @@
+/*
+ * grunt-init-fcoo-application
+ * https://gruntjs.com/
+ *
+ */
+
+'use strict';
+
+// Basic template description.
+exports.description = 'Create a FCOO repository for a application';
+
+// Template-specific notes to be displayed before question prompts.
+exports.notes = 'Please enter following information:';
+
+
+// Template-specific notes to be displayed after question prompts.
+exports.after = 
+	'*******************************************\n' +
+	'You should now run the following commands\n' +
+	'>bower update\n' +
+	'>npm install\n' +
+	'>grunt dev\n' +
+	'or >bower update & npm install & grunt dev (on Windows)\n' +
+	'or >bower update ; npm install ; grunt dev (on Linux)\n' +
+	'*******************************************\n' +
+	'';
+
+// Any existing file or directory matching this wildcard will cause a warning.
+exports.warnOn = '*';
+
+// The actual init template.
+exports.template = function(grunt, init, done) {
+  init.process({type: 'jquery'}, [
+	
+		init.prompt('name'),
+
+		init.prompt('description (from README.md)'),
+/*
+		init.prompt('github_user'),
+		init.prompt('version'),
+    init.prompt('repository'),
+    init.prompt('homepage'),
+*/
+    init.prompt('author_name'),
+
+		init.prompt('author_email'),
+
+  ], function(err, props) {
+
+
+		//Add default values
+		props.licenses = ['MIT'];
+		props.year = (new Date()).getFullYear();
+
+    // Files to copy (and process).
+    var files = init.filesToCopy(props);
+
+    // Add properly-named license files.
+    init.addLicenseFiles(files, props.licenses);
+
+    // Actually copy (and process) files.
+    init.copyAndProcess(files, props);
+
+    // All done!
+    done();
+	});
+
+};
